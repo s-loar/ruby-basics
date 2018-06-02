c7e4c1c1b021aa56eeaaa5936c9388906ea02a01
Steven Loar <sjl7909@gmail.com>
2018-06-02 15:11:43 -0400

    Add reset method to gadget class

--- gadget.rb
+++ gadget.rb
@@ -1,13 +1,14 @@
 class Gadget
 
   # attr_writer :password
-  attr_reader :production_number
+  attr_reader :production_number, :apps
   attr_accessor :username
 
   def initialize(username, password)
     @username = username
     @password = password
     @production_number = generate_production_number
+    @apps = []
   end
 
   def to_s
@@ -20,7 +21,15 @@ class Gadget
     @password = new_password if validate_password(new_password)
   end
 
+  def reset(username, password)
+    self.username = username
+    self.password = password
+    self.apps = []
+  end
+
   private
+
+    attr_writer :apps
 
     def generate_production_number
       start_digits = rand(10000..99999)
