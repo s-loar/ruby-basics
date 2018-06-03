bc24ee4e0442d2a296a0a66f09679f6d66ce43e2
Steven Loar <sjl7909@gmail.com>
2018-06-03 14:21:27 -0400

    Super from subclasses

--- subclasses.rb
+++ subclasses.rb
@@ -18,19 +18,42 @@ end
 # puts jake.introduce
 
 class Manager < Employee
+
+  def initialize(name, age, rank)
+    super(name, age)
+    @rank = rank
+  end
+
+  def introduce
+    super + "\nI am a #{@rank}. Woo-hoo!"
+  end
+
+  def yell
+    "Who's the Boss? I'm the boss!"
+  end
 end
 
 class Worker < Employee
+  def clock_in(time)
+    "Starting my shift at #{time}"
+  end
+
+  def yell
+    "I'm working! I'm working!"
+  end
 end
 
-jake = Manager.new("Jake", 31)
-p jake
+jake = Manager.new("Jake", 31, "Vice President")
+# p jake
 puts jake.introduce
+# puts jake.yell
 
-dan = Worker.new("Dan", 22)
-puts dan.introduce
+# dan = Worker.new("Dan", 22)
+# puts dan.introduce
+# puts dan.clock_in(Time.now)
+# puts dan.yell
 
-puts jake.is_a?(Manager)
-puts jake.is_a?(Employee)
-puts jake.instance_of?(Manager)
-puts jake.instance_of?(Employee)
+# puts jake.is_a?(Manager)
+# puts jake.is_a?(Employee)
+# puts jake.instance_of?(Manager)
+# puts jake.instance_of?(Employee)
